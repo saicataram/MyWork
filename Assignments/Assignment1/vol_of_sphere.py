4ed5694182d6809dc6de5c2c270e9071deee9816
saicataram <sai.cataram@gmail.com>
2020-04-23 23:00:47 +0530

    Assignments Folder

---
+++ Assignments/Assignment1/vol_of_sphere.py
@@ -0,0 +1,14 @@
+""" 
+*************************************************************************************
+Author: SK
+Date: 23-Apr-2020
+Description: Python Assignments for practice
+************************************************************************************* 
+"""
+
+pi = 3.1415926535897931
+diameter= input("Enter Diameter value: ")
+radius = int(diameter)/2
+print("Radius of Sphere is: ", int(radius))
+volume = 4.0/3.0*pi* int(radius)**3
+print("The volume of the sphere is: ",volume)
