12ed94b02759f0b0ef8dcf6c3e3aeb3ca6659f3b
saicataram <sai.cataram@gmail.com>
2020-06-30 06:52:27 +0530

    updated predict file

--- ml-framework/src/train.py
+++ ml-framework/src/train.py
@@ -8,6 +8,7 @@ import joblib
 from . import dispatcher
 
 TRAINING_DATA = os.environ.get("TRAINING_DATA")
+TEST_DATA = os.environ.get("TEST_DATA")
 FOLD = int(os.environ.get("FOLD"))
 MODEL = os.environ.get("MODEL")
 
@@ -21,6 +22,7 @@ FOLD_MAPPING = {
 
 if __name__ == "__main__":
     df = pd.read_csv(TRAINING_DATA)
+    df_test = pd.read_csv(TEST_DATA)
     train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
     valid_df = df[df.kfold == FOLD]
 
@@ -32,13 +34,13 @@ if __name__ == "__main__":
 
     valid_df = valid_df[train_df.columns]
 
-    label_encoders = []
+    label_encoders = {}
     for c in train_df.columns:
         lbl = preprocessing.LabelEncoder()
-        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
+        lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist() + df_test[c].values.tolist())
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoders.append((c, lbl))
+        label_encoders[c] = lbl
 
     # data is ready to train
     clf = dispatcher.MODELS[MODEL]
@@ -46,5 +48,6 @@ if __name__ == "__main__":
     preds = clf.predict_proba(valid_df()[:, 1])
     print(metrics.roc_auc_score(yvalid, preds))
     
-    joblib.dump(label_encocders, f"models/{MODEL}_label_encoder.pkl")
-    joblib.dump(clf, f"models/{MODEL}.pkl")
+    joblib.dump(label_encoders, f"models/{MODEL}_{FOLD}_label_encoder.pkl")
+    joblib.dump(clf, f"models/{MODEL}_{FOLD}.pkl")
+    joblib.dump(train_df.columns, f"models/{MODEL}_{FOLD}_columns.pkl")
